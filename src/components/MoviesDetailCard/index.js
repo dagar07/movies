abd8aceb8a32caf98c3b6dfdc526752c85aa6ec3
tilak <tilak.panchal@pasarpolis.com>
2020-06-17 21:35:54 +0530

    add dummy image for images

--- src/components/MoviesDetailCard/index.js
+++ src/components/MoviesDetailCard/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { DisplayFlex } from '../../styles/commonStyle'
 import { MoviesCardStyle } from './index.style'
+import { DUMMY_POSTER } from '../../images'
 
 class MoviesDetailCard extends Component {
   static propTypes = {
@@ -10,8 +11,9 @@ class MoviesDetailCard extends Component {
 
   render() {
     const { movie = {} } = this.props
+    
     return (
-      <MoviesCardStyle imgUrl={movie.Poster}>
+      <MoviesCardStyle imgUrl={movie.Poster.includes('http') ? movie.Poster : DUMMY_POSTER}>
         <div className='bottom-strip'>
           <div className='movie-title'>{movie.Title}</div>
           <DisplayFlex>
---
+++ src/images/index.js
@@ -0,0 +1,1 @@
+export const DUMMY_POSTER = `${window.location.origin}/film-bkg.jpg`
