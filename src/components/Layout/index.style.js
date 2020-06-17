48521678df3a8547e3639a629f79ec958960bda0
tilak <tilak.panchal@pasarpolis.com>
2020-06-17 08:59:47 +0530

    add the color

--- src/components/Layout/index.style.js
+++ src/components/Layout/index.style.js
@@ -2,10 +2,5 @@ import styled from 'styled-components'
 
 export const LayoutStyle = styled.div`
   min-height: 100vh;
-  background-color: ${props => props.theme.lightGrey};
-
-  .children-container {
-    max-width: 1200px;
-    margin: 0 auto;
-  }
+  background-color: ${props => props.theme.homeBg};
 `
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,6 +1,8 @@
 export const COLOR = {
   primary: '#2C3E50',
   secondary: '#566573',
+  homeBg: '#1b2938',
+  headerBg: '#152029',
   white: '#ffffff',
   black: '#000000',
   grey: '#aaaaaa',
